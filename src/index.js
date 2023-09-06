a78dbd16a9f8afac4606c4305d539a20a9331793
MaruFelis <amaruse123@gmail.com>
2023-09-06 15:21:04 -0300

    Calculadora JavaScript
    Ya esta la funcionalidad :de:

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 let sonido = new Audio();
 sonido.src="../public/sounds/sonido.mp3";
+let respuestaPorcentaje = document.getElementById('respuestaPorcentaje');
+let respuestaTotal = document.getElementById('respuestaTotal');
 
-let monto = 4000;
-let porcentaje = 50;
-let propina;
+function sacarPropina() {
+    let monto = parseFloat(document.getElementById("monto").value);
+    let porcentaje = parseFloat(document.getElementById("porcentaje").value);
 
-function init(){
-    propina = (porcentaje / 100)*monto;
+    if (isNaN(monto) || isNaN(porcentaje)) {
+        alert("Ingrese un monto y un porcentaje válidos.");
+        return;
+    }
+
+    let propina = (porcentaje / 100) * monto;
+    let total = propina + monto;
+    respuestaPorcentaje.innerHTML = `El porcentaje es: ${propina.toFixed(2)}`;
+    respuestaTotal.innerHTML = `El total es: ${total.toFixed(2)}`;
+    console.log(total);
     console.log(propina);
 }
